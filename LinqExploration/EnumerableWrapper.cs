c5fe7e86a15117f36209f8f7c8dd6d2d516004bc
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-05-19 23:57:31 +0100

    Added a class count number of calls to IEnumerator<T> methods

---
+++ LinqExploration/EnumerableWrapper.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LinqExploration
+{
+    public class EnumerableWrapper<T> : IEnumerable<T>
+    {
+        private readonly IEnumerator<T> _enumerator;
+
+        public EnumerableWrapper(IEnumerable<T> sequence)
+        {
+            _enumerator = sequence.GetEnumerator();
+            NumCallsToMoveNext = 0;
+            NumCallsToCurrent = 0;
+            NumCallsToReset = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int NumCallsToMoveNext { get; private set; }
+        public int NumCallsToCurrent { get; private set; }
+        public int NumCallsToReset { get; private set; }
+
+        public void ResetCallCounts()
+        {
+            NumCallsToMoveNext = 0;
+            NumCallsToCurrent = 0;
+            NumCallsToReset = 0;
+        }
+
+        public bool MoveNext()
+        {
+            NumCallsToMoveNext++;
+            return _enumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            NumCallsToReset++;
+            _enumerator.Reset();
+        }
+
+        public T Current
+        {
+            get
+            {
+                NumCallsToCurrent++;
+                return _enumerator.Current;
+            }
+        }
+
+        public class Enumerator : IEnumerator<T>
+        {
+            private readonly EnumerableWrapper<T> _enumerableWrapper;
+
+            public Enumerator(EnumerableWrapper<T> enumerableWrapper)
+            {
+                _enumerableWrapper = enumerableWrapper;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return _enumerableWrapper.MoveNext();
+            }
+
+            public void Reset()
+            {
+                _enumerableWrapper.Reset();
+            }
+
+            public T Current
+            {
+                get { return _enumerableWrapper.Current; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
+    }
+}
---
+++ LinqExploration/Quantifiers/Any.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace LinqExploration.Quantifiers
+{
+    [TestFixture]
+    internal class Any
+    {
+        [Test]
+        public void AnyOnlyEnumeratesTheSequenceAsFarAsTheFirstMatch()
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var enumerableWrapper = new EnumerableWrapper<int>(numbers);
+            var actual = enumerableWrapper.Any(n => n >= 5);
+            Assert.That(actual, Is.True);
+            Assert.That(enumerableWrapper.NumCallsToMoveNext, Is.EqualTo(5));
+        }
+    }
+}
