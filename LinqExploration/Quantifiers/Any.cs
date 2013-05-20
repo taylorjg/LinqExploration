e46a74d63d89878067430a12afeced26a8bae44f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-05-20 10:28:27 +0100

    Renamed EnumerableWrapper to EnumerableSpy

--- LinqExploration/Quantifiers/Any.cs
+++ LinqExploration/Quantifiers/Any.cs
@@ -7,15 +7,41 @@ namespace LinqExploration.Quantifiers
     internal class Any
     {
         [Test]
-        public void AnyOnlyEnumeratesTheSequenceAsFarAsTheFirstMatch()
+        public void AnyWithoutAPredicateOnlyEnumeratesTheFirstItemInTheSequence()
         {
-            var numbers = Enumerable.Range(1, 10);
-            var enumerableWrapper = new EnumerableWrapper<int>(numbers);
-            var actual = enumerableWrapper.Any(n => n >= 5);
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            var actual = enumerableSpy.Any();
             Assert.That(actual, Is.True);
-            Assert.That(enumerableWrapper.NumCallsToGetEnumerator, Is.EqualTo(1));
-            Assert.That(enumerableWrapper.NumCallsToMoveNext, Is.EqualTo(5));
-            Assert.That(enumerableWrapper.NumCallsToDispose, Is.EqualTo(1));
+            Assert.That(enumerableSpy.NumCallsToGetEnumerator, Is.EqualTo(1));
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(1));
+            Assert.That(enumerableSpy.NumCallsToDispose, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AnyWithAPredicateOnlyEnumeratesTheSequenceAsFarAsTheFirstMatch()
+        {
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            var actual = enumerableSpy.Any(n => n >= 5);
+            Assert.That(actual, Is.True);
+            Assert.That(enumerableSpy.NumCallsToGetEnumerator, Is.EqualTo(1));
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(5));
+            Assert.That(enumerableSpy.NumCallsToDispose, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AnyWithAPredicateEnumeratesTheEntireSequenceIfNoMatchFound()
+        {
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            var itemCount = enumerableSpy.Count();
+            enumerableSpy.ResetCallCounts();
+            var actual = enumerableSpy.Any(n => n >= 99);
+            Assert.That(actual, Is.False);
+            Assert.That(enumerableSpy.NumCallsToGetEnumerator, Is.EqualTo(1));
+
+            // The 11th call to MoveNext() returns false because we have reached the end of the sequence.
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(itemCount + 1));
+
+            Assert.That(enumerableSpy.NumCallsToDispose, Is.EqualTo(1));
         }
     }
 }
