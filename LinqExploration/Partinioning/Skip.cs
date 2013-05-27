1e1ae69addf3ba29b4b35bab79135e419d0af573
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-05-27 12:22:14 +0100

    Added Skip and SkipWhile tests

---
+++ LinqExploration/Partinioning/Skip.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace LinqExploration.Partinioning
+{
+    [TestFixture]
+    internal class Skip
+    {
+        [Test]
+        public void SkipEnumeratesElementsInTheSequenceUntilTheCountIsReached()
+        {
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            var actual = enumerableSpy.Skip(4);
+            Assert.That(actual.First(), Is.EqualTo(5));
+            Assert.That(enumerableSpy.NumCallsToGetEnumerator, Is.EqualTo(1));
+
+            // We test for 4 + 1 because 4 is the number of elements that we skipped.
+            // We then took the first element in the remainder of the sequence.
+            // We have to do something with the remainder of the sequence in order
+            // to get past the delayed execution of the query.
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(4 + 1));
+
+            Assert.That(enumerableSpy.NumCallsToDispose, Is.EqualTo(1));
+            Assert.That(actual, Is.EquivalentTo(new[] { 5, 6, 7, 8, 9, 10 }));
+        }
+    }
+}
---
+++ LinqExploration/Partinioning/SkipWhile.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace LinqExploration.Partinioning
+{
+    [TestFixture]
+    internal class SkipWhile
+    {
+        [Test]
+        public void SkipWhileEnumeratesElementsInTheSequenceUntilThePredicateReturnsFalse()
+        {
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            var actual = enumerableSpy.SkipWhile(i => i < 7);
+            Assert.That(actual.First(), Is.EqualTo(7));
+            Assert.That(enumerableSpy.NumCallsToGetEnumerator, Is.EqualTo(1));
+
+            // I had expected this to be 7 + 1.
+            // We need to reach the 7th element in the sequence to find an
+            // element for which the predicate returns false. Then, we take
+            // the first element in the remainder of the sequence.
+            // But, internally, SkipWhile uses a SkipWhileIterator which is
+            // smart. As soon as it reaches the first element that doesn't
+            // satisfy the predicate, it starts returning elements.
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(6 + 1));
+
+            Assert.That(enumerableSpy.NumCallsToDispose, Is.EqualTo(1));
+        }
+    }
+}
