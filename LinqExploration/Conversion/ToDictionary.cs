ea474951f061fe94150e32e97eba332dcdb4540f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-01 13:08:34 +0100

    Changed the tests involving a comparer to do something more useful

--- LinqExploration/Conversion/ToDictionary.cs
+++ LinqExploration/Conversion/ToDictionary.cs
@@ -1,5 +1,6 @@
-﻿using System.Linq;
-using LinqExploration.AlbumData.EqualityComparers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace LinqExploration.Conversion
@@ -18,6 +19,7 @@ namespace LinqExploration.Conversion
 
             // Assert
             Assert.That(actual.Count(), Is.EqualTo(5));
+            Assert.That(actual.Comparer, Is.EqualTo(EqualityComparer<string>.Default));
             Assert.That(actual["So What"], Is.SameAs(tracks.ElementAt(0)));
             Assert.That(actual["Freddy Freeloader"], Is.SameAs(tracks.ElementAt(1)));
             Assert.That(actual["Blue in Green"], Is.SameAs(tracks.ElementAt(2)));
@@ -32,15 +34,16 @@ namespace LinqExploration.Conversion
             var tracks = AlbumData.AlbumData.Artists.First().Albums.First().Tracks;
 
             // Act
-            var actual = tracks.ToDictionary(t => t.Title, t => t.TrackNumber);
+            var actual = tracks.ToDictionary(t => t.TrackNumber, t => t.Title);
 
             // Assert
             Assert.That(actual.Count(), Is.EqualTo(5));
-            Assert.That(actual["So What"], Is.EqualTo(1));
-            Assert.That(actual["Freddy Freeloader"], Is.EqualTo(2));
-            Assert.That(actual["Blue in Green"], Is.EqualTo(3));
-            Assert.That(actual["All Blues"], Is.EqualTo(4));
-            Assert.That(actual["Flamenco Sketches"], Is.EqualTo(5));
+            Assert.That(actual.Comparer, Is.EqualTo(EqualityComparer<int>.Default));
+            Assert.That(actual[1], Is.EqualTo("So What"));
+            Assert.That(actual[2], Is.EqualTo("Freddy Freeloader"));
+            Assert.That(actual[3], Is.EqualTo("Blue in Green"));
+            Assert.That(actual[4], Is.EqualTo("All Blues"));
+            Assert.That(actual[5], Is.EqualTo("Flamenco Sketches"));
         }
 
         [Test]
@@ -50,16 +53,16 @@ namespace LinqExploration.Conversion
             var tracks = AlbumData.AlbumData.Artists.First().Albums.First().Tracks;
 
             // Act
-            var comparer = new TrackLengthEqualityComparer();
-            var actual = tracks.ToDictionary(t => t, comparer);
+            var actual = tracks.ToDictionary(t => t.Title, StringComparer.CurrentCultureIgnoreCase);
 
             // Assert
             Assert.That(actual.Count(), Is.EqualTo(5));
-            Assert.That(actual[tracks.ElementAt(0)], Is.SameAs(tracks.ElementAt(0)));
-            Assert.That(actual[tracks.ElementAt(1)], Is.SameAs(tracks.ElementAt(1)));
-            Assert.That(actual[tracks.ElementAt(2)], Is.SameAs(tracks.ElementAt(2)));
-            Assert.That(actual[tracks.ElementAt(3)], Is.SameAs(tracks.ElementAt(3)));
-            Assert.That(actual[tracks.ElementAt(4)], Is.SameAs(tracks.ElementAt(4)));
+            Assert.That(actual.Comparer, Is.EqualTo(StringComparer.CurrentCultureIgnoreCase));
+            Assert.That(actual["so what"], Is.SameAs(tracks.ElementAt(0)));
+            Assert.That(actual["freddy freeloader"], Is.SameAs(tracks.ElementAt(1)));
+            Assert.That(actual["blue in green"], Is.SameAs(tracks.ElementAt(2)));
+            Assert.That(actual["all blues"], Is.SameAs(tracks.ElementAt(3)));
+            Assert.That(actual["flamenco sketches"], Is.SameAs(tracks.ElementAt(4)));
         }
 
         [Test]
@@ -69,16 +72,16 @@ namespace LinqExploration.Conversion
             var tracks = AlbumData.AlbumData.Artists.First().Albums.First().Tracks;
 
             // Act
-            var comparer = new TrackLengthEqualityComparer();
-            var actual = tracks.ToDictionary(t => t, t => t.TrackNumber, comparer);
+            var actual = tracks.ToDictionary(t => t.Title, t => t.TrackNumber, StringComparer.CurrentCultureIgnoreCase);
 
             // Assert
             Assert.That(actual.Count(), Is.EqualTo(5));
-            Assert.That(actual[tracks.ElementAt(0)], Is.EqualTo(1));
-            Assert.That(actual[tracks.ElementAt(1)], Is.EqualTo(2));
-            Assert.That(actual[tracks.ElementAt(2)], Is.EqualTo(3));
-            Assert.That(actual[tracks.ElementAt(3)], Is.EqualTo(4));
-            Assert.That(actual[tracks.ElementAt(4)], Is.EqualTo(5));
+            Assert.That(actual.Comparer, Is.EqualTo(StringComparer.CurrentCultureIgnoreCase));
+            Assert.That(actual["so what"], Is.EqualTo(1));
+            Assert.That(actual["freddy freeloader"], Is.EqualTo(2));
+            Assert.That(actual["blue in green"], Is.EqualTo(3));
+            Assert.That(actual["all blues"], Is.EqualTo(4));
+            Assert.That(actual["flamenco sketches"], Is.EqualTo(5));
         }
     }
 }
