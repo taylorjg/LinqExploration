1aab6d07371de8513173f276a422e5945cfb8b0c
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-02 22:09:32 +0100

    Added tests for First and FirstOrDefault

---
+++ LinqExploration/Element/First.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+// ReSharper disable ReturnValueOfPureMethodIsNotUsed
+
+namespace LinqExploration.Element
+{
+    [TestFixture]
+    internal class First
+    {
+        [Test]
+        public void FirstOnlyEnumeratesTheFirstElementInTheSequence()
+        {
+            var enumerableSpy = new EnumerableSpy<int>(Enumerable.Range(1, 10));
+            enumerableSpy.First();
+            Assert.That(enumerableSpy.NumCallsToMoveNext, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FirstWithoutPredicate()
+        {
+            var actual = SampleData.Artists.First();
+            Assert.That(actual.Name, Is.EqualTo("Miles Davis"));
+        }
+
+        [Test]
+        public void FirstWithPredicate()
+        {
+            var actual = SampleData.Artists.First(artist => artist.Albums.Any(album => album.Tracks.Count() > 5));
+            Assert.That(actual.Name, Is.EqualTo("Bill Evans"));
+        }
+
+        [Test]
+        public void FirstThrowsExceptionIfNoElementInTheSequenceSatisfiesThePredicate()
+        {
+            Assert.Throws<System.InvalidOperationException>(() => SampleData.Artists.First(artist => artist.Albums.Any(album => album.Tracks.Count() > 10)));
+        }
+    }
+}
---
+++ LinqExploration/Element/FirstOrDefault.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using LinqExploration.AlbumData;
+using NUnit.Framework;
+
+// ReSharper disable PossibleNullReferenceException
+
+namespace LinqExploration.Element
+{
+    [TestFixture]
+    internal class FirstOrDefault
+    {
+        [Test]
+        public void FirstOrDefaultWithoutPredicate()
+        {
+            var actual = SampleData.Artists.FirstOrDefault();
+            Assert.That(actual.Name, Is.EqualTo("Miles Davis"));
+        }
+
+        [Test]
+        public void FirstOrDefaultWithPredicate()
+        {
+            var actual = SampleData.Artists.FirstOrDefault(artist => artist.Albums.Any(album => album.Tracks.Count() > 5));
+            Assert.That(actual.Name, Is.EqualTo("Bill Evans"));
+        }
+
+        [Test]
+        public void FirstOrDefaultWithoutPredicateReturnsDefaultIfTheSequenceIsEmpty()
+        {
+            var actual = Enumerable.Empty<Artist>().FirstOrDefault();
+            Assert.That(actual, Is.SameAs(default(Artist)));
+        }
+
+        [Test]
+        public void FirstOrDefaultWithPredicateReturnsDefaultIfNoElementIsFoundThatSatisfiesThePredicate()
+        {
+            var actual = SampleData.Artists.FirstOrDefault(artist => artist.Albums.Any(album => album.Tracks.Count() > 10));
+            Assert.That(actual, Is.SameAs(default(Artist)));
+        }
+    }
+}
