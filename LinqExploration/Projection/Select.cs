d686df1a4c203fc167219efde692e7f6db40cb21
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-02 18:33:01 +0100

    Added more asserts to the Select tests

--- LinqExploration/Projection/Select.cs
+++ LinqExploration/Projection/Select.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using LinqExploration.AlbumData;
+﻿using System.Linq;
 using NUnit.Framework;
 
 namespace LinqExploration.Projection
@@ -11,15 +9,21 @@ namespace LinqExploration.Projection
         [Test]
         public void SelectWithoutIndex()
         {
-            var actual = SampleData.Artists.Select(artist => artist.Name);
-            Assert.That(actual.Count(), Is.EqualTo(SampleData.Artists.Count()));
+            var actual = SampleData.Artists.Select(artist => artist.Name).ToList();
+            Assert.That(actual.Count, Is.EqualTo(SampleData.Artists.Count()));
+            Assert.That(actual[0], Is.EqualTo("Miles Davis"));
+            Assert.That(actual[1], Is.EqualTo("Bill Evans"));
         }
 
         [Test]
         public void SelectWithIndex()
         {
-            var actual = SampleData.Artists.Select((artist, index) => new {artist.Name, Index = index});
-            Assert.That(actual.Count(), Is.EqualTo(SampleData.Artists.Count()));
+            var actual = SampleData.Artists.Select((artist, index) => new {artist.Name, Index = index}).ToList();
+            Assert.That(actual.Count, Is.EqualTo(SampleData.Artists.Count()));
+            Assert.That(actual[0].Name, Is.EqualTo("Miles Davis"));
+            Assert.That(actual[0].Index, Is.EqualTo(0));
+            Assert.That(actual[1].Name, Is.EqualTo("Bill Evans"));
+            Assert.That(actual[1].Index, Is.EqualTo(1));
         }
 
         [Test]
@@ -28,6 +32,11 @@ namespace LinqExploration.Projection
             var actual = SampleData.Artists.Select(artist => artist.Albums.Select(album => album.Tracks)).ToList();
             Assert.That(actual.First().Count(), Is.EqualTo(SampleData.Artists.First().Albums.Count()));
             Assert.That(actual.First().First().Count(), Is.EqualTo(SampleData.Artists.First().Albums.First().Tracks.Count()));
+            Assert.That(actual.First().First().ElementAt(0).TrackNumber, Is.EqualTo(1));
+            Assert.That(actual.First().First().ElementAt(1).TrackNumber, Is.EqualTo(2));
+            Assert.That(actual.First().First().ElementAt(2).TrackNumber, Is.EqualTo(3));
+            Assert.That(actual.First().First().ElementAt(3).TrackNumber, Is.EqualTo(4));
+            Assert.That(actual.First().First().ElementAt(4).TrackNumber, Is.EqualTo(5));
         }
     }
 }
